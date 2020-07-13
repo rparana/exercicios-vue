bc010b53cb332dbf98607a9709dbd98eb9be6a1c
Rafael Paraná <paranafael@yahoo.com.br>
2020-07-13 12:25:39 -0300

    Criado e adicionado Projeto Calculadora

---
+++ calculator/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+  import Calculator from "@/main/Calculator.vue";
+  export default {
+    components: {
+      Calculator,
+    },
+    name: "App",
+  };
+</script>
+
+<style>
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: #f3904f; /* fallback for old browsers */
+    background: -webkit-linear-gradient(to left, #3b4371, #f3904f); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to left, #3b4371, #f3904f); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+</style>
